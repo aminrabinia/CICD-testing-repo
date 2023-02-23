2a979c86ac38de332b1e4722b335e91349990715
amin rabinia <amin.rabinia@gmail.com>
2023-02-23 15:00:28 -0800

    main updated

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ app = FastAPI()
 def root():
     return {"message": "program runs successfully!!!"}
 
+@app.get('/testmsg')
+def root():
+    return {"message": "welcome to test!!!"}
+
 
 if __name__ == "__main__":
 	uvicorn.run(app, host='0.0.0.0', port=8080)
